a6476eed1bad9aa1ea4297869b04989770cbb99c
ARTG-17 <72416294+ARTG-17@users.noreply.github.com>
2020-11-16 18:06:24 +0000

    subtask 1

--- src/calculator.java
+++ src/calculator.java
@@ -97,6 +97,60 @@ public class calculator {
 		} 
 		while (con.equals("Y"));
 		scan.close();
+	
+	try (//aadil
+	Scanner scan1 = new Scanner(System.in)) {
+		double input1, input2, ans;
+		int selection;
+		System.out.println("Enter 2 numbers: ");
+		input1=scan1.nextDouble();
+		input2=scan1.nextDouble();
+		//allows user to input two numbers
+		
+		System.out.println("Choose what operator you want to use: ");
+		System.out.println("1. addition: ");
+		System.out.println("2. subtraction: ");
+		System.out.println("3. division: ");
+		System.out.println("4. multiplication: ");
+		//User selects what operator they want to use
+		
+		selection=scan1.nextInt();
+		while(selection>4) {
+			System.out.println("enter the correct selection");
+			selection=scan1.nextInt();
+		}
+		//gives user chances to choose the proper operator
+		
+		Operator thing =new Operator();
+		if(selection==1) {
+			ans=thing.add(input1,input2);
+			System.out.println("your answer is "+ans);
+		}else if(selection==2) {
+			ans=thing.sub(input1,input2);
+			System.out.println("your answer is "+ans);
+		}else if(selection==3) {
+			ans=thing.div(input1,input2);
+			System.out.println("your answer is "+ans);
+		}else if(selection==4) {
+			ans=thing.multi(input1,input2);
+			System.out.println("your answer is "+ans);
+		}
+		//aadil
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
 }
 //comment
