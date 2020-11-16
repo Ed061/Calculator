36bcb430d0be7fff9ea28d8b61b11ba56ffded18
Ed061 <2018701@brunel.ac.uk>
2020-11-16 20:59:33 +0200

    added operator bit

--- src/Operator.java
+++ src/Operator.java
@@ -1,4 +1,27 @@
 
 public class Operator {
+	public double add(double x, double y) {
+        double z;
+        z=x+y;
+        return z;
 
+    }
+    public double sub(double x, double y) {
+        double z;
+        z=x-y;
+        return z;
+
+    }
+    public double multi(double x, double y) {
+        double z;
+        z=x*y;
+        return z;
+
+    }
+    public double div(double x, double y) {
+        double z;
+        z=x/y;
+        return z;
+
+    }
 }
