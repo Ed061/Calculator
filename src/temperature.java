719287e61265c0a1f7f945da09dc473a52a074d9
Divy <2011492@brunel.ac.uk>
2020-11-19 17:26:44 +0000

    .

--- src/temperature.java
+++ src/temperature.java
@@ -1,25 +1,7 @@
 import java.util.Scanner;
 
 public class temperature {
-//	public static void main(String[] args) {
-//        System.out.println("This Program will allow the user to calculate temperature.");
-//
-//	
-//        
-//        Scanner ob = new Scanner(System.in);
-//        System.out.println("Please enter a 1 to convert Fahrenheit to Celsius.");
-//        System.out.println("Please enter a 2 to convert Celsius to Fahrenheit.");
-//        String option = ob.next();
-//        if (option.equals("1")){
-//            System.out.println("Please enter a temperature in degrees Fahrenheit.");
-//            cel();
-//        }else if (option.equals("2")){
-//            System.out.println("Please enter a temperature in degrees Celsius.");
-//            fah();
-//        }else{
-//            System.out.println("ERROR!");
-//        }
-//    }
+
     public static void cel() {
         Scanner ob = new Scanner(System.in);
         double celsius = ob.nextInt();
@@ -31,5 +13,24 @@ public class temperature {
         double fahrenheit = ob.nextInt();
         System.out.println(((fahrenheit - 32) * (5 / 9)) + " Celsius");
     }
+//	public static void main(String[] args) {
+//  System.out.println("This Program will allow the user to calculate temperature.");
+//
+//
+//  
+//  Scanner ob = new Scanner(System.in);
+//  System.out.println("Please enter a 1 to convert Fahrenheit to Celsius.");
+//  System.out.println("Please enter a 2 to convert Celsius to Fahrenheit.");
+//  String option = ob.next();
+//  if (option.equals("1")){
+//      System.out.println("Please enter a temperature in degrees Fahrenheit.");
+//      cel();
+//  }else if (option.equals("2")){
+//      System.out.println("Please enter a temperature in degrees Celsius.");
+//      fah();
+//  }else{
+//      System.out.println("ERROR!");
+//  }
+//}
 }
 ///sdsads/a/d/asd/
