fc12d81ed326c90cb0d340f169e3efc91b5583b0
Ed061 <2018701@brunel.ac.uk>
2020-12-08 00:22:37 +0200

    Added decimal to binary

--- src/calculator.java
+++ src/calculator.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class calculator {
-	//Power, modulo, and square/cube roots 
+
 	public static double power(double num1, double num2) {
-		// alternative method but int
+
 		double ans = 1;
 		while(num2 != 0){
 			ans *= num1;
@@ -28,20 +28,28 @@ public class calculator {
 		
 		return Math.cbrt(num1);
 	}
-//Divy -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	public static double celsius(double num1) {
-		return ((num1*1.8)+32);
-	}
-	public static double fahrenhiet(double num1) {
-		return ((num1 -32 ) * 5/9);
+	
+	public static String DtoB(int decimal) { //converting decimal to binary
+		
+		int i = 0;
+		int binary[] = new int[100];
+		String bin = "";
+		
+		while(decimal != 0) {
+			// This finds out the binary number of the decimal but it is stored in reverse order in array
+			binary[i] = decimal % 2;
+			decimal /= 2;
+			++i;
+		}
+		
+		// Reading the elements stored in the array in reverse order to get binary digits in right order
+		for (i-=1; i>=0; i--) {
+			bin += binary[i];
+		}
+		return bin;
 	}
 	
-	public static double BtoD(double num1) {
-		
-	}
-//Divy -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-	public static String tohex(int num) {
+	public static String tohex(int num) {//converting decimal to hexadecimal
 		int rem = 0;
 		char hexchars[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 		String conv = "";
@@ -52,6 +60,19 @@ public class calculator {
 		}
 		return conv;
 	}
+	
+//Divy -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	public static double celsius(double num1) {
+		return ((num1*1.8)+32);
+	}
+	public static double fahrenhiet(double num1) {
+		return ((num1 -32 ) * 5/9);
+	}
+	
+//	public static double BtoD(double num1) {
+		
+//	}
+//Divy -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	
 //Aadil -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	
@@ -133,35 +154,36 @@ public class calculator {
 		 System.out.println("[14] Convert Imperial into Metric for mass");
 		 System.out.println("[15] Convert Imperial into Metric for length");
 		 System.out.println("[16] Convert Binary to Decimal");
+		 System.out.println("[17] Convert Decimal to Binary");
 		 double num1 = 0, num2 = 0;
 		 //int choice = 0;
 		
 		 String choice = scan.nextLine();
 		 //doing the selected operations
 		 switch(choice) {
-		 	case "1":
+		 	case "1"://power
 		 		System.out.println("Enter your numbers");
 		 		num1 = scan.nextDouble();
 		 		num2 = scan.nextDouble();
 		 		System.out.println(power(num1, num2));
 		 		break;
-		 	case "2":
+		 	case "2"://modulo
 		 		System.out.println("Enter your numbers");
 		 		num1 = scan.nextDouble();
 		 		num2 = scan.nextDouble();
 		 		System.out.println(modulus(num1, num2));
 		 		break;
-		 	case "3":
+		 	case "3"://square root
 		 		System.out.println("Enter your number");
 		 		num1 = scan.nextInt();
 		 		System.out.println(sqroot(num1));
 		 		break;
-		 	case "4":
+		 	case "4"://Cube root
 		 		System.out.println("Enter your number");
 		 		num1 = scan.nextInt();
 		 		System.out.println(cbroot(num1));
 		 		break;
-		 	case "5":
+		 	case "5"://Convert number to hex
 		 		System.out.println("Enter your number");
 		 		int hexnr;
 		 		hexnr = scan.nextInt();
@@ -328,6 +350,12 @@ public class calculator {
 		 		System.out.println("Enter your length");	 
 		 		num2 = scan.nextDouble();
 		 		imperial1(num2);
+		 		break;
+		 	case "17"://convert Decimal to Binary
+		 		System.out.print("Enter a decimal number: ");
+		 		int decimal;
+		 		decimal = scan.nextInt();
+		 		System.out.println(DtoB(decimal));
 		 		break;
 //Shayiq -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 			 			 	
